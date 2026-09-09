a4fd82bc1b8388d5ccdc3468e0dd1d98baffc534
Guheshp <guheshpanjagall481@gmail.com>
2026-09-09 19:48:08 +0530

    Add frontend CI/CD deployment

--- src/routes/PlanRoute.jsx
+++ src/routes/PlanRoute.jsx
@@ -1,7 +1,3 @@
-import CreateLead from "../pages/leads/Createlead";
-import LeadEdit from "../pages/leads/LeadEdit";
-import Leads from "../pages/leads/Leads";
-import LeadView from "../pages/leads/LeadView";
 import CreatePlan from "../pages/plans/CreatePlan";
 import PlanEdit from "../pages/plans/PlanEdit";
 import Plans from "../pages/plans/Plans";
@@ -19,18 +15,6 @@ const planRoutes = [
     path: "plans/edit/:id",
     element: <PlanEdit />,
   },
-  //   {
-  //     path: "leads/edit/:id",
-  //     element: <LeadEdit />,
-  //   },
-  //   {
-  //     path: "users/:id",
-  //     element: <UserDetails />,
-  //   },
-  //   {
-  //     path: "users/:id/edit",
-  //     element: <EditUser />,
-  //   },
 ];
 
 export default planRoutes;
